e046fb20bdb78ac92f384a3d1ca3b77ec5dec9f6
João Pedro <joao_giacometti@hotmail.com>
2020-07-06 16:29:08 -0300

    Starting to implement Board and Piece

--- ChessGame/src/application/Program.java
+++ ChessGame/src/application/Program.java
@@ -1,9 +1,12 @@
 package application;
 
+import boardgame.Board;
+
 public class Program {
 
 	public static void main(String[] args) {
-	
+		
+		
 	}
 
 }
--- ChessGame/src/boardgame/Position.java
+++ ChessGame/src/boardgame/Position.java
@@ -5,13 +5,9 @@ public class Position {
 	private int row;
 	private int column;
 	
-	
-	public Position() {
-		
-	}
+	public Position() {}
 	
 	public Position(int row, int column) {
-		
 		this.row = row;
 		this.column = column;
 	}
