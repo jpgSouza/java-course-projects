2646385e0e1a64f8b6ba21c9f4d1fc4d8f9c7b9e
João Pedro <joao_giacometti@hotmail.com>
2020-07-29 11:57:05 -0300

    Department entity class

---
+++ DemoDaoJDBC/src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Department department = new Department(1, "Books");
+		System.out.println(department);
+		
+	}
+
+}
