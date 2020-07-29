5d5ae21df1eab92de612b6651d3f235f2f651a23
João Pedro <joao_giacometti@hotmail.com>
2020-07-29 12:11:26 -0300

    Seller entity class

--- DemoDaoJDBC/src/application/Program.java
+++ DemoDaoJDBC/src/application/Program.java
@@ -1,13 +1,19 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Department department = new Department(1, "Books");
-		System.out.println(department);
+		
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, department);
+		
+		System.out.println(seller);
 		
 	}
 
