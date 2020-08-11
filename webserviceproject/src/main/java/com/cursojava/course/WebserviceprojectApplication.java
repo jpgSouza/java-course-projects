b8d02fcd6bf23a69cabf77b66af911e1b822bb6b
João Pedro <joao_giacometti@hotmail.com>
2020-08-11 11:20:39 -0300

    Creating WebService Project

---
+++ webserviceproject/src/main/java/com/cursojava/course/WebserviceprojectApplication.java
@@ -0,0 +1,13 @@
+package com.cursojava.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebserviceprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebserviceprojectApplication.class, args);
+	}
+
+}
