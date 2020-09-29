86b2689600b26dbeaa5f4dd145c13e5b73bc7cbc
João Pedro <joao_giacometti@hotmail.com>
2020-09-29 14:05:35 -0300

    Category

--- webserviceproject/src/main/java/com/cursojava/course/config/TestConfig.java
+++ webserviceproject/src/main/java/com/cursojava/course/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.cursojava.course.entities.Category;
 import com.cursojava.course.entities.Order;
 import com.cursojava.course.entities.User;
 import com.cursojava.course.entities.enums.OrderStatus;
+import com.cursojava.course.repositories.CategoryRepository;
 import com.cursojava.course.repositories.OrderRepository;
 import com.cursojava.course.repositories.UserRepository;
 
@@ -24,6 +26,9 @@ public class TestConfig implements CommandLineRunner{
 	@Autowired
 	private OrderRepository orderRepository;
 
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		
@@ -33,9 +38,14 @@ public class TestConfig implements CommandLineRunner{
 		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.DELIVERED, u1);
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.PAID, u2);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1); 
-	
+		
+		Category c1 = new Category(null, "Eletronics");
+		Category c2 = new Category(null, "Books");
+		Category c3 = new Category(null, "Computers");
+		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
+		categoryRepository.saveAll(Arrays.asList(c1,c2,c3));
 		
 	}
 	
